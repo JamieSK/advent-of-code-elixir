c6faae2301b1662ab8d952eddcc7b35d6ebdf4f1
Jamie Klein <jamie.klein@multiverse.io>
2024-12-04 09:44:02 +0000

    2024 day 3

--- lib/advent_of_code/solution/year_2024/day_03.ex
+++ lib/advent_of_code/solution/year_2024/day_03.ex
@@ -1,7 +1,17 @@
 defmodule AdventOfCode.Solution.Year2024.Day03 do
-  def part1(_input) do
+  def part1(input) do
+    ~r/mul\((\d+),(\d+)\)/ 
+    |> Regex.scan(input, capture: :all_but_first)
+    |> Enum.map(&Enum.map(&1, fn s -> String.to_integer(s) end))
+    |> Enum.map(&Enum.product/1)
+    |> Enum.sum()
   end
 
-  def part2(_input) do
+  def part2(input) do
+    ~r/do\(\)/
+    |> Regex.split(input)
+    |> Enum.map(fn x -> hd(Regex.split(~r/don't\(\)/, x)) end)
+    |> Enum.map(&part1/1)
+    |> Enum.sum()
   end
 end
--- test/advent_of_code/solution/year_2024/day_03_test.exs
+++ test/advent_of_code/solution/year_2024/day_03_test.exs
@@ -6,21 +6,24 @@ defmodule AdventOfCode.Solution.Year2024.Day03Test do
   setup do
     [
       input: """
+      xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))
+      """,
+      input_2: """
+      xmul(2,4)&mul[3,7]!^don't()_mul(5,5)don't()+mul(32,64]
+      (mul(11,8)undo()?mul(8,5)don't()mul(10,10))do()
       """
     ]
   end
 
-  @tag :skip
   test "part1", %{input: input} do
     result = part1(input)
 
-    assert result
+    assert result == 161
   end
 
-  @tag :skip
-  test "part2", %{input: input} do
+  test "part2", %{input_2: input} do
     result = part2(input)
 
-    assert result
+    assert result == 48
   end
 end
